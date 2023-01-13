c34fc83b04a97a82dbec6a980c71291ac05e8b40
Nick Hong <me@nickhong.xyz>
2023-01-13 02:53:17 -0500

    Fix attribute names

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -12,10 +12,10 @@ const Video = () => {
         <div className='border-white border-b-2 w-full relative bottom-3' />
       </div>
       <div className='py-12 relative pt-10 h-4/5 max-w-screen-lg mx-auto sm:mt-20'>
-      <iframe src="https://player.vimeo.com/video/781320182?h=db24612c0a&color=eaeae5&title=0&byline=0&portrait=0" 
-        width="100%" height="100%" frameborder="0" 
-        allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
-
+      <iframe src="https://player.vimeo.com/video/781320182?h=db24612c0a&color=eaeae5&title=0&byline=0&portrait=0"
+        width="100%" height="100%" frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen>
       </iframe>
       </div>
     </section>
