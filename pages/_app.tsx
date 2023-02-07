1bcaed1f0ccaedcf1bd98646b3a727852e21b982
Nick Hong <me@nickhong.xyz>
2023-02-07 01:51:18 -0500

    add ga analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../src/components/Layout'
 import ContractContextProvider from '../src/components/ContractContext';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContractContextProvider>
       <Layout>
+        <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
       </Layout>
     </ContractContextProvider>
