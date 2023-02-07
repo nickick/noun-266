c35fbe869d6016d18d67471409878b935212f4ae
Nick Hong <me@nickhong.xyz>
2023-02-07 01:01:55 -0500

    Update styling of button on mobile

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -12,7 +12,7 @@ const ConnectButton = ({
   return (
     <div
       onClick={connectWallet}
-      className={`rounded-xl w-1/2 sm:w-full mx-auto my-6 sm:my-0 px-6 py-2 ${matchMintButton ? 'sm:py-4' : 'sm:py-0'} cursor-pointer text-center bg-[#DF30A8]`}
+      className={`sm:w-full mx-auto my-6 sm:my-0 px-6 ${matchMintButton ? 'rounded-lg py-4 sm:py-4 w-full' : 'rounded-2xl py-2 sm:py-0 w-1/2'} cursor-pointer text-center bg-[#DF30A8]`}
     >
       {!currentAccount
         ? matchMintButton ? 'Mint' : 'Connect'
